6f341c88890b167b2066c3b906cac92f180377b1
Eugene233 <agboreugene@gmail.com>
2025-07-26 17:29:11 +0200

    Remove debug statement

--- service/resources/wikidata/lexeme.py
+++ service/resources/wikidata/lexeme.py
@@ -200,7 +200,6 @@ class LexemesGet(Resource):
 class LexemesTranslate(Resource):
     def post(self):
         request_body = request.get_json()
-        print(request_body)
         if not request_body:
             abort(400, 'Request body is empty')
 
