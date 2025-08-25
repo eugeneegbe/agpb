e038720a99e4fe7fbdfb06be0bca40c9dcb031a4
Eugene233 <agboreugene@gmail.com>
2025-08-25 11:37:23 +0200

    Compare languages for search

--- service/resources/wikidata/utils.py
+++ service/resources/wikidata/utils.py
@@ -113,8 +113,10 @@ def lexemes_search(search, src_lang, ismatch):
     search_result_data = process_search_results(wd_search_results['search'],
                                                 search, src_lang, bool(ismatch))
 
+
+    language_label = get_language_label(getLanguages(), src_lang).lower()
     data = [item for item in search_result_data \
-            if src_lang == item["language"]]
+            if language_label == item["description"].split()[0].lower().strip('(),')]
 
     return data
 
